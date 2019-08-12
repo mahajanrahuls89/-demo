074ecaf6b9f0fcde070aa7cc3693f416981dca9a
Rahul M <mahajanrahuls89@gmail.com>
2019-08-12 14:08:17 +0530

    Create ReconfigureEngine3.sh

---
+++ ReconfigureEngine3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+directorName=$1
+directorIP=$2
+directorPort=$3
+INSTALLDIR=$4
+sslenabled=$5
+
+printf "\n${directorIP} ${directorName}" > director.txt
+cat director.txt >> /etc/hosts
+
+cd $INSTALLDIR
+
+printf "\n${directorIP} ${directorName}" >> director.txt
+printf "sslEnabled: ${sslenabled}" >> director.txt
+
+cd ./datasynapse/engine
+
+if [ "${sslenabled}" == "true" ]
+then
+    printf "\nsslenabled == true " >> director.txt
+    ./configure.sh -s ${directorName}:${directorPort} -l y
+else
+    printf "\nsslenabled == false " >> director.txt
+   ./configure.sh -s ${directorName}:${directorPort}
+fi
+
+./engine.sh start
