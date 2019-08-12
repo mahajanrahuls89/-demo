f4831f4190f9efaa0c8ff1cf5c1a04a8b81cd20d
Rahul M <mahajanrahuls89@gmail.com>
2019-08-12 14:41:43 +0530

    Update ReconfigureEngine3.sh

--- ReconfigureEngine3.sh
+++ ReconfigureEngine3.sh
@@ -15,13 +15,13 @@ printf "sslEnabled: ${sslenabled}" >> director.txt
 
 cd ./datasynapse/engine
 
-if [ "$sslenabled" == "true" ]
-    then
+if [ $5 == 'true' ]
+then
         printf "\nsslenabled == true " >> director.txt
         ./configure.sh -s ${directorName}:${directorPort} -l y
 else
         printf "\nsslenabled == false " >> director.txt
-       ./configure.sh -s ${directorName}:${directorPort}
+        ./configure.sh -s ${directorName}:${directorPort}
 fi
 
 ./engine.sh start
