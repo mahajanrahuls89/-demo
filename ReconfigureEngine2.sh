7e75a97b2cafc86429e67aca940cd5edbb8baeb0
Rahul M <mahajanrahuls89@gmail.com>
2019-08-12 12:00:29 +0530

    Create ReconfigureEngine2.sh

---
+++ ReconfigureEngine2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+directorName=$1
+directorIP=$2
+directorPort=$3
+INSTALLDIR=$4
+sslenabled=$5
+
+printf "\n${directorIP} ${directorName}" > director.txt
+cat director.txt >> /etc/hosts
+
+cd $INSTALLDIR
+
+
+
+cd ./datasynapse/engine
+
+if [ "${sslenabled}" = "true"]
+then
+    ./configure.sh -s ${directorName}:${directorPort} -l y
+else
+   ./configure.sh -s ${directorName}:${directorPort}
+fi
+
+./engine.sh start
+
