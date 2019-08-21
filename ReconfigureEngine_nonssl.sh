8971b6d20c1675a5894635da2dcd7af4aa155493
Rahul M <mahajanrahuls89@gmail.com>
2019-08-21 12:23:24 +0530

    Create ReconfigureEngine_nonssl.sh

---
+++ ReconfigureEngine_nonssl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+directorName=$1
+directorIP=$2
+directorPort=$3
+INSTALLDIR=$4
+
+cat director.txt >> /etc/hosts
+
+cd $INSTALLDIR
+
+printf "\n${directorIP}  == ${directorName}" >> directorsss.txt
+
+printf "\n${directorPort}" >> directorsss.txt
+cd ./datasynapse/engine
+./configure.sh -s ${directorName}:${directorPort} -l y
+
+./engine.sh start
